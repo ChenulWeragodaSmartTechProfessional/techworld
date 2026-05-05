72a8ecf34d27c44d7da3abe8eb2355ab532fa19e
Chenul Weragoda <138956930+ChenulWeragodaSmartTechProfessional@users.noreply.github.com>
2026-05-05 20:04:36 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10967659
+    "zoneId": 10967687
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
